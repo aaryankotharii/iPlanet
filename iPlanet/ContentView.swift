369ad4f17bdde537f39f514647e28e95e05a6eb4
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-08-04 12:03:00 +0530

    Initial Commit

---
+++ iPlanet/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iPlanet
+//
+//  Created by Aaryan Kothari on 04/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iPlanet/iPlanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPlanetApp.swift
+//  iPlanet
+//
+//  Created by Aaryan Kothari on 04/08/20.
+//
+
+import SwiftUI
+
+@main
+struct iPlanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
