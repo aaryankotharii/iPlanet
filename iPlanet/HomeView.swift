b899281fc16112168e32985f431cdcc100268efa
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-08-04 12:19:21 +0530

    Added: home view

---
+++ iPlanet/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  iPlanet
+//
+//  Created by Aaryan Kothari on 04/08/20.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
