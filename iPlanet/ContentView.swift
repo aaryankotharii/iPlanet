23a1835fbcf546b7d1ae0949f4eead00b6362ad1
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-13 19:12:20 +0530

    HomeView basic setup complete

---
+++ iPlanet/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  iPlanet
+//
+//  Created by Aaryan Kothari on 13/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
